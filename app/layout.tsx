37af518a9cba2127327f05f12a67fb241e896e78
Rohit Shukla <shuklarohit2105@gmail.com>
2024-12-31 18:09:04 +0530

    Change default theme in ThemeProvider from 'system' to 'light'

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body className="antialiased font-sans">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
